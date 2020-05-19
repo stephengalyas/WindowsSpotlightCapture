1c3154d5e2567664a4dd5c130c75ee73c7bd6b51
Stephen Galyas <stephen.galyas@student.fairfield.edu>
2020-05-18 21:24:56 -0400

    Begin working on configuration class.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WindowsSpotlightCapture.Objects;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +29,20 @@ namespace WindowsSpotlightCapture
         public MainPage()
         {
             this.InitializeComponent();
+            MessageDialog md;
+            
+            if (Configuration.IsFirstLaunch)
+            {
+                md = new MessageDialog("This is the first time you have launched this app!", "First Launch");
+            }
+            else
+            {
+                md = new MessageDialog("This is not the first time you have launched this app!", "Not First Launch");
+            }
+
+            Task<bool> tskInitialize = Configuration.Initalize();
+            IAsyncOperation<IUICommand> uiAction = md.ShowAsync();
+            
         }
     }
 }
---
+++ objects/Configuration.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Windows.Storage;
+using Windows.Foundation;
+using Windows.System.Diagnostics.TraceReporting;
+using Windows.UI.Xaml.Documents;
+using System.Reflection.Metadata.Ecma335;
+using System.Diagnostics;
+using Windows.Media.Streaming.Adaptive;
+using System.Runtime.CompilerServices;
+
+namespace WindowsSpotlightCapture.Objects
+{
+    /// <summary>
+    /// A class used to store configuration data and method.
+    /// </summary>
+    internal static class Configuration
+    {
+        /// <summary>
+        /// Returns true if the application data directory does not exist for this user.
+        /// </summary>
+        internal static bool IsFirstLaunch { get { return !Directory.Exists(AppDataDirectory); } }
+        /// <summary>
+        /// The application directory.
+        /// </summary>
+        internal static string AppDataDirectory { get { return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WindowsSpotlightCapture\"; } }
+        /// <summary>
+        /// App trace listener name.
+        /// </summary>
+        private static string TraceListenerName { get { return "WSPTracer"; } }
+        /// <summary>
+        /// Initializes the program.
+        /// </summary>
+        /// <param name="error">(out) The error message.</param>
+        /// <returns>True if successful, otherwise false.</returns>
+        internal static async Task<bool> Initalize()
+        {
+            bool success = true;
+            string error = string.Empty;
+            StorageFolder sf;
+            Task<StorageFile> tskCreateFile = null;
+            if (IsFirstLaunch)
+            {  
+                // Create application directory.
+                try
+                {
+                    Directory.CreateDirectory(AppDataDirectory);
+                }
+                catch (Exception exc)
+                {
+                    success = false;
+                    error = "We had a problem setting up this program for first use.\n\n" + (exc.GetType()).Name + " - " + exc.Message;
+                }
+
+                if(!success)
+                {
+                    return success;
+                }
+
+                // Create app data directory.
+                sf = await StorageFolder.GetFolderFromPathAsync(Configuration.AppDataDirectory);
+
+                // Create file asynchronously.
+                tskCreateFile = sf.CreateFileAsync("asyncfile.txt").AsTask<StorageFile>();
+
+                // Initalize tracer.
+                success = InitalizeTracer(out error);
+
+                // Wait for file to be created.
+                await tskCreateFile;
+            }
+            return success;
+        }
+        /// <summary>
+        /// Creates a trace listener for this application.
+        /// </summary>
+        /// <param name="error">(out) If fail, the error message.</param>
+        /// <returns>True if successful, otherwise false.</returns>
+        private static bool InitalizeTracer(out string error)
+        {
+            bool success = true;
+            bool exists = false;
+            error = string.Empty;
+            foreach (TraceListener tl in Trace.Listeners)
+            {
+                if(tl.Name == Configuration.TraceListenerName)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (!exists)
+            {
+                try
+                {
+                    Trace.Listeners.Add(new TextWriterTraceListener(Configuration.TraceListenerName));
+                }
+                catch (Exception exc)
+                {
+                    success = false;
+                    error = "We had a problem with our logging feature.\n\n" + (exc.GetType()).Name + " - " + exc.Message;
+                }
+            }
+
+            return success;
+        }   // Close InitializeTracer().
+
+    }
+}
